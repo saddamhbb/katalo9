cf79592133e4488eea04f8f0a2ee390304533849
Saddam Habibie <saddam.habibie@spacestock.com>
2021-04-23 22:11:45 +0800

    Add Logo Icon
    - Update Logo Components so it have a flower picture
    - Update Logo Component Styling

--- src/components/Logo/index.js
+++ src/components/Logo/index.js
@@ -4,6 +4,7 @@ import "./style.scss"
 const Logo = () => {
     return (
         <div className="logo">
+            <img className="logo-icon" src="/img/flower.svg" title="Flower Shop Logo" alt="Flower Shop Logo" />
             Flowers
         </div>
     )
