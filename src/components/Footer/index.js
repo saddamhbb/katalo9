394c46ec9d389985d755f6946e8a00ecfe681756
Saddam Habibie <saddam.habibie@spacestock.com>
2021-04-23 22:19:59 +0800

    Update Footer Styling

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
     return (
-        <div>Copyright 2021</div>
+        <div className="p-1 text-center text-white bg-primary">Copyright &copy; 2021</div>
     )
 }
 
